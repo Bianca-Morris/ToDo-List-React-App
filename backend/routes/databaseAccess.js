32a06fdf8761c3812ca5afa1973eabb8e28a6989
Bianca-Morris <bmorris@wellesley.edu>
2017-10-08 12:58:00 -0700

    Connect react app to mongoDB, express server

--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const TodoItem = require('../models/TodoItem.js');
+
+console.log("backend/routes/databaseAccess.js")
 
 router.get('/add', (req, res)=>{
-  res.send('Hello');
+  const testTodo = new TodoItem({
+    task: "test task"
+  });
+  testTodo.save()
+  .then (response => {res.send(response);})
+  .catch (error => {res.send(error)})
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
+const mongoose = require('mongoose');
+
+mongoose.connection.on('connected', ()=> console.log("MongoDB connected!"));
+if (!process.env.MONGODB_URI){
+  throw new Error ("No URI provided. Please source env.sh.")
+}
+mongoose.connect(process.env.MONGODB_URI);
+
+
 
 app.use(express.static('build'));
 app.use('/db', dbRoutes);
